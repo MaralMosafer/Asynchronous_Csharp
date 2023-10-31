cc3038c0241090b81e77142e28c18a8485a1854e
Maral <mrlmosafercomputer@gmail.com>
2023-10-31 16:20:44 +0330

    Finish WinForm example

---
+++ AwaitWinForm/SimpleWinForm_Project/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SimpleWinForm_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Download_btn = new Button();
+            result_lbl = new Label();
+            SuspendLayout();
+            // 
+            // Download_btn
+            // 
+            Download_btn.Location = new Point(59, 38);
+            Download_btn.Name = "Download_btn";
+            Download_btn.Size = new Size(75, 23);
+            Download_btn.TabIndex = 0;
+            Download_btn.Text = "button1";
+            Download_btn.UseVisualStyleBackColor = true;
+            Download_btn.Click += Download_btn_Click;
+            // 
+            // result_lbl
+            // 
+            result_lbl.AutoSize = true;
+            result_lbl.Location = new Point(280, 153);
+            result_lbl.Name = "result_lbl";
+            result_lbl.Size = new Size(38, 15);
+            result_lbl.TabIndex = 1;
+            result_lbl.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(result_lbl);
+            Controls.Add(Download_btn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Download_btn;
+        private Label result_lbl;
+    }
+}
---
+++ AwaitWinForm/SimpleWinForm_Project/Form1.cs
@@ -0,0 +1,49 @@
+﻿namespace SimpleWinForm_Project
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient;
+        public Form1(HttpClient httpClient = null)
+        {
+            InitializeComponent();
+            _httpClient = httpClient ?? new HttpClient();
+        }
+
+        private async void Download_btn_Click(object sender, EventArgs e)
+        {
+            Download_btn.Enabled = false;
+            result_lbl.Text = "Receiving information...";
+            try
+            {
+                string result = await DownloadDataAsync();
+
+                result_lbl.Text = "information Reciveed! ✔️";
+                MessageBox.Show(result, "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                result_lbl.Text = "Error! ❌";
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Download_btn.Enabled = true;
+            }
+        }
+        private async Task<string> DownloadDataAsync()
+        {
+            // Use HttpClient to get information from a URL
+            string url = "https://swapi.dev/api/people/1/";
+            HttpResponseMessage response = await _httpClient.GetAsync(url); // Calling an HTTP request asynchronously
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new Exception("Error in receiving information!");
+            }
+        }
+    }
+}
